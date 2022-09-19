c0546c01b97eb058c8c2716d2c7f3ab63630087f
levilimas <leviimediata@hotmail.com>
2022-09-19 20:18:51 -0300

    Revert "Configurados e criados Crud de veículos"
    This reverts commit 24e38322f1568c40694d249f5fe9f080343b9610.

--- prettier.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  endOfLine: 'auto',
-  printWidth: 120,
-  singleQuote: true,
-  semi: true,
-  htmlWhitespaceSensitivity: 'ignore',
-  trailingComma: 'all',
-};
--- src/server.js
+++
@@ -1,3 +0,0 @@
-import app from "./app";
-const PORT = 3000;
-app.listen(PORT, () => console.log(`\nRunning at http://localhost:${PORT}\n`));
