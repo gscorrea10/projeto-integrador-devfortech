3aa3df2149619d18a24e3019e1a1aa284a80fedb
Gabriel Corrêa <gscorrea95@gmail.com>
2022-09-22 19:43:29 -0300

    fix

--- projeto_integrador/src/routes/routes.js
+++ projeto_integrador/src/routes/routes.js
@@ -1,12 +1,13 @@
-import { Router } from "express";
-import { userRouter } from "./user.routes.js";
-import { sessionsRouter } from "./sessions.routes.js";
-import { billsRouter } from "./bills.routes.js";
-import { processRouter } from "./process.routes.js";
-import { processRouter } from "./vehicles.routes.js";
+import { Router } from 'express';
+import { userRouter } from './user.routes.js';
+import { sessionsRouter } from './sessions.routes.js';
+import { billsRouter } from './bills.routes.js';
+import { processRouter } from './process.routes.js';
+import { vehiclesRouter } from './vehicles.routes.js';
 
 export const routes = Router();
 routes.use('/users', userRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/bills', billsRouter);
 routes.use('/process', processRouter);
+routes.use('/vehicles', vehiclesRouter);
