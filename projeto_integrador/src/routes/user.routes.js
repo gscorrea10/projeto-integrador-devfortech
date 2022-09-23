2dc389451f43f4db6abf53a1723adf478401af81
Gabriel Corrêa <gscorrea95@gmail.com>
2022-09-23 18:57:43 -0300

    quick fix

--- projeto_integrador/src/routes/user.routes.js
+++ projeto_integrador/src/routes/user.routes.js
@@ -6,10 +6,12 @@ import {
   validationBodyUpdateUser,
   checkUpdateUser,
 } from '../middlewares/validators/user.validator';
+import AuthTokenMiddleware from '../middlewares/authToken.middleware.js';
 
 export const userRouter = Router();
 
 userRouter.post('/', validationBodyCreateUser, checkCreateUser, UsersController.create);
+userRouter.use(AuthTokenMiddleware.verifyToken);
 userRouter.get('/', UsersController.index);
 userRouter.get('/:id', UsersController.retrieve);
 userRouter.patch('/:id', validationBodyUpdateUser, checkUpdateUser, UsersController.update);
